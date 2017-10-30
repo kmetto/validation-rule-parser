f2463c8f1ec490c2e5120089e20cf4b00c4219b6
Konstantin Metto <konstantin.metto@codeswat.com>
2017-10-30 10:52:28 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+class RulesParser {
+    /**
+     * 
+     * @param {string} rulesStr 
+     */
+    constructor(rulesStr) {
+        this.rules = [];
+        this.rulesStr = rulesStr;
+        this.compiledRules = {};
+        this.compileRules();
+    }
+
+    compileRules() {
+        const rawRules = this.rulesStr.split('|');
+        if (rawRules.length > 0) {
+            this.parseRules(rawRules);
+        }
+    }
+
+    /**
+     * 
+     * @param {array} rawRules 
+     */
+    parseRules(rawRules) {
+        rawRules.forEach((rowRule) => {
+            let rule;
+            let rawParams;
+
+            [rule, ...rawParams] = rowRule.split(":");
+
+            if (!rawParams.length) {
+                this.compiledRules[this.normalize(rule)] = true;
+                return;
+            }
+
+            this.parseParams(this.normalize(rule), rawParams);
+
+        });
+    }
+
+    /**
+     * 
+     * @param {string} rawParams 
+     */
+    parseParams(rule, rawParams) {
+        rawParams.forEach(rawParam => {
+            if (rule === 'format') {
+                this.compiledRules[rule] = rawParam;
+            } else if (rawParam.indexOf(';') === -1) {
+                const parsedParamsValues = this.parseParamValues(rule, rawParam);
+                if (typeof parsedParamsValues === 'object') {
+                    let name;
+                    let value;
+                    ({ name, value } = parsedParamsValues);
+                    this.compiledRules[rule] = {
+                        [name]: value };
+                } else {
+                    this.compiledRules[rule] = parsedParamsValues;
+                };
+            } else {
+                let parsedParamsValues = {};
+                let name;
+                let value;
+
+                rawParam.split(';').forEach((rawParamValue) => {
+                    ({ name, value } = this.parseParamValues(rule, rawParamValue));
+                    parsedParamsValues[name] = value;
+                });
+
+                this.compiledRules[rule] = parsedParamsValues;
+            }
+        });
+    }
+
+    parseParamValues(rule, rawParam) {
+        if (rawParam.indexOf('=') === -1) {
+            return this.normalize(rawParam);
+        } else {
+            let param = {};
+            let name;
+            let value;
+
+            [name, value] = rawParam.split('=');
+            name = this.normalize(name);
+            value = this.normalize(value);
+            param.name = name;
+            param.value = value;
+            return param;
+        }
+    }
+
+    parseFormatValues(rawParam) {
+        if (rawParam.indexOf('=') === -1) {
+            return this.normalize(rawParam);
+        }
+    }
+
+    /**
+     * 
+     * @param {string} str 
+     */
+    normalize(str) {
+        if (isFinite(str)) {
+            return parseFloat(str);
+        } else if (str === 'true' || str === 'false') {
+            return str === 'true';
+        } else if (typeof str === 'string') {
+            return str.toLowerCase().replace(/^\s+|\s+$/g, '');
+        }
+
+
+    }
+
+    getCompiled() {
+        return this.compiledRules;
+    }
+}
+
+export default RulesParser;
---
+++ test/test.js
@@ -0,0 +1,42 @@
+require("babel-register");
+const assert = require('chai').assert;
+const RulesParser = require('../index').default;
+
+describe("Validation rule parser", () => {
+    it('presence', () => {
+        const rules = new RulesParser('presence');
+        assert.deepEqual(rules.getCompiled(), { presence: true });
+    });
+    it('mail', () => {
+        const rules = new RulesParser('mail');
+        assert.deepEqual(rules.getCompiled(), { mail: true });
+    });
+    it('mail:true', () => {
+        const rules = new RulesParser('mail:true');
+        assert.deepEqual(rules.getCompiled(), { mail: true });
+    });
+    it('mail|precense', () => {
+        const rules = new RulesParser('mail|presence');
+        assert.deepEqual(rules.getCompiled(), { mail: true, presence: true });
+    });
+    it('length:1', () => {
+        const rules = new RulesParser('length:1');
+        assert.deepEqual(rules.getCompiled(), { length: 1 });
+    });
+    it('length:minimum=1', () => {
+        const rules = new RulesParser('length:minimum=1');
+        assert.deepEqual(rules.getCompiled(), { length: { minimum: 1 } });
+    });
+    it('length:minimum=1;maximum=10', () => {
+        const rules = new RulesParser('length:minimum=1;maximum=10');
+        assert.deepEqual(rules.getCompiled(), { length: { minimum: 1, maximum: 10 } });
+    });
+    it('format:[a-z0-9]+', () => {
+        const rules = new RulesParser('format:[a-z0-9]+');
+        assert.deepEqual(rules.getCompiled(), { format: '[a-z0-9]+' });
+    });
+    it('format:[a-z0-9]+|length:minimum=1;maximum=10|mail', () => {
+        const rules = new RulesParser('format:[a-z0-9]+|length:minimum=1;maximum=10|mail');
+        assert.deepEqual(rules.getCompiled(), { format: '[a-z0-9]+', length: { minimum: 1, maximum: 10 }, mail: true });
+    });
+});
